66a258cdfac02072bb616ba52fdb17810b3012a9
alfreding0 <alfredogonzales953@gmail.com>
2021-08-29 13:54:56 -0400

    agregado Personal controller

--- app/Http/Controllers/PersonaController.php
+++ app/Http/Controllers/PersonaController.php
@@ -11,4 +11,42 @@ class PersonaController extends Controller
         $personas = Persona::all();  //select * from persona;
         return view('persona.index', compact('personas'));
     }
+
+    public function create(){
+        return view('persona.create');
+    }
+
+    public function store(Request $request){
+        $persona = new Persona();
+        $persona->nombre = $request->input('nombre');
+        $persona->apellido = $request->input('apellido');
+        $persona->direccion = $request->input('direccion');
+        $persona->telefono = $request->input('telefono');
+        $persona->save();
+
+        return redirect()->route('persona.index');
+    }
+
+    public function edit($id){
+        $persona = Persona::findOrFail($id);
+        return view('persona.edit', compact('persona'));
+    }
+
+    public function update(Request $request, $id){
+        $persona = Persona::findOrFail($id);
+        $persona->nombre = $request->input('nombre');
+        $persona->apellido = $request->input('apellido');
+        $persona->direccion = $request->input('direccion');
+        $persona->telefono = $request->input('telefono');
+        $persona->save();
+
+        return redirect()->route('persona.index');
+    }
+
+    public function destroy($id){
+        $persona = Persona::findOrFail($id);
+        $persona->delete();
+
+        return redirect()->route('persona.index');
+    }
 }
