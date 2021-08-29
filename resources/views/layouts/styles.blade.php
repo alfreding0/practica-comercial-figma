03ee42d17dd9c50f430f6b27ffa0795991c9bccd
alfreding0 <alfredogonzales953@gmail.com>
2021-08-29 13:52:06 -0400

    agregado el framework materialize al proyecto

--- resources/views/layouts/styles.blade.php
+++ resources/views/layouts/styles.blade.php
@@ -4,7 +4,6 @@
 <!-- icons -->
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-
 <!-- Compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
