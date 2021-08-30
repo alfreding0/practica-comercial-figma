de46a7768666eb9b2968f553b0a14f1796bad940
alfreding0 <alfredogonzales953@gmail.com>
2021-08-30 18:53:59 -0400

    configurando el login para iniciar sesion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,30 @@
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Alquiler</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alquiler</title>
 
-        @include('layouts.styles')
-        @stack('styles')
-    </head>
+    @include('layouts.styles')
+    @stack('styles')
+</head>
 
-    <body>
-        @include('layouts.header')
-        <main class="primary-background">
-            <div class="container"></div>
-            <div class="row">
-                <div class="col s12 m12 l12 l12 xl10 offset-xl1">
-                    @include('layouts.response')
-                    @yield('content')
-                </div>
-            </div>
+<body>
+@include('layouts.header')
+<main class="primary-background">
+    <div class="container"></div>
+    <div class="row">
+        <div class="col s12 m12 l12 l12 xl10 offset-xl1">
+            @include('layouts.response')
+            @yield('content')
+        </div>
+    </div>
 
-            @include('layouts.preloader')
-            @include('layouts.footer')
-        </main>
+    @include('layouts.preloader')
+    @include('layouts.footer')
+</main>
 
-        @include('layouts.scripts')
-        @stack('scripts')
-    </body>
+@include('layouts.scripts')
+@stack('scripts')
+</body>
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,10 +7,19 @@
 <nav class="navbar nav-extended no-padding dark-primary-color">
     <div class="nav-wrapper">
         @auth
-        <a href="#" class="brand-logo center hide-on-small-only">Curso de laravel</a>
+        <a href="#" class="brand-logo center hide-on-small-only">COMERCIAL FIGMA</a>
         <ul id="nav-mobile" class="right">
             <li><a href="{{ route('home') }}" class="waves-effect dark-primary-color-text" type="submit"><i class="material-icons">person</i></a></li>
-            <li><a href="{{ route('logout') }}" class="waves-effect dark-primary-color-text" type="submit">Cerrar sesión</a></li>
+            <li>
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();">
+                   Salir
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
         </ul>
         <a href="#!" data-target="sidenav-left" class="sidenav-trigger left show-on-medium-and-up"><i class="material-icons dark-primary-color-icon">menu</i></a>
 
