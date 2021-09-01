0a29147c5b8bec700fe1b63ec2d200e437e3593b
alfreding0 <alfredogonzales953@gmail.com>
2021-09-01 03:44:01 -0400

    cambiado un poco el diseño del menusidenav

--- resources/views/layouts/menu-sidenav.blade.php
+++ resources/views/layouts/menu-sidenav.blade.php
@@ -1,6 +1,18 @@
-<ul id="sidenav-left" class="sidenav">
-    <li><div class="user-view dark-primary-color">
-            <a href="#user"><img class="circle" src="{{ asset('imgs/perfil.png') }}"></a>
+<ul id="sidenav-left" class="sidenav pink lighten-4">
+    <li>
+        <div class="user-view center">
+            <div class="background">
+                <img src="https://images.pexels.com/photos/6765581/pexels-photo-6765581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" 
+                alt=""
+                class="responsive-img">
+            </div>
+
+            <div class="row">
+
+                <div class="col s9 push-s3 center-align">
+                    <a href="#user" class="centrado"><img class="circle responsive-img" src="{{ asset('imgs/perfil.png') }}"></a>
+                </div>
+            </div>
             <a href="#name"><span class="white-text name">{{ auth()->user()->persona->nombre.' '.auth()->user()->persona->apellido }}</span></a>
             <a href="#email"><span class="white-text email">{{ auth()->user()->email }}</span></a>
         </div>
